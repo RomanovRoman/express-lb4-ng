156c3253dab8f6d719f60b86ed124ed8c91157f7
dougal83 <dougal83+git@gmail.com>
2019-03-10 17:00:22 +0000

    mount graphql server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,9 @@ import {ApplicationConfig} from '@loopback/core';
 import * as express from 'express';
 import * as path from 'path';
 import * as http from 'http';
+import * as fs from 'fs';
+import * as graphqlHTTP from 'express-graphql';
+import {createGraphQlSchema} from 'oasgraph';
 
 export class ExpressServer {
   private app: express.Application;
@@ -14,6 +17,33 @@ export class ExpressServer {
     this.lbApp = new ServerApplication(options);
     // host lb4 @ /api
     this.app.use('/api', this.lbApp.requestHandler);
+
+    // mounting graphql endpoint using the middleware express-graphql
+
+    // can we obtain openapi.json from lbApp?  .getApiSpec() is empty at this point
+    // openapi.json manually placed
+    const myschema = JSON.parse(
+      fs.readFileSync(path.resolve('./dist/openapi.json'), 'utf8'),
+    );
+    createGraphQlSchema(myschema, {
+      strict: false,
+      viewer: true,
+      addSubOperations: true,
+      fillEmptyResponses: true,
+    })
+      .then(({schema}) => {
+        this.app.use(
+          '/graphql',
+          graphqlHTTP({
+            schema: schema,
+            graphiql: true,
+          }),
+        );
+      })
+      .catch(err => {
+        console.log('graphql creation event error: ', err.message);
+      });
+
     // host angular files
     this.app.use(express.static(path.resolve('./dist/public')));
   }
